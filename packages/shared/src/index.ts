320e2545686d9db33772a74106a89149db36853e
Ethan Wang <ethan.wang1993@outlook.com>
2026-03-03 18:15:42 +0800

    feat: shared

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,6 +1,14 @@
-export const isObject = (val: unknown): boolean =>
-  val !== null && typeof val === "object";
+export function isObject(value) {
+  return typeof value === "object" && value !== null;
+}
 
 export function isFunction(value) {
   return typeof value == "function";
 }
+export function isString(value) {
+  return typeof value == "string";
+}
+export * from "./shapeFlags";
+
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+export const hasOwn = (value, key) => hasOwnProperty.call(value, key);
