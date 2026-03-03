4a14cbbdde3ac5401d784b3ec142835f8b062168
Ethan Wang <ethan.wang1993@outlook.com>
2026-03-03 18:15:16 +0800

    feat: runtime-dom

---
+++ packages/runtime-dom/src/patchProp.ts
@@ -0,0 +1,24 @@
+// 主要是对节点元素的属性操作 class style event 普通属性
+import patchAttr from "./modules/attr";
+import patchClass from "./modules/class";
+import patchEvent from "./modules/event";
+import patchStyle from "./modules/style";
+
+// diff
+
+// div onClick=fn1     div click=fn2
+
+// div onClick=()=>invoker.value()
+
+export default function patchProp(el, key, prevValue, nextValue) {
+  if (key === "class") {
+    return patchClass(el, nextValue);
+  } else if (key === "style") {
+    return patchStyle(el, prevValue, nextValue);
+  } else if (/^on[^a-z]/.test(key)) {
+    // el.addEventListener(key,prevValue)  // ()=> nextValue()
+    return patchEvent(el, key, nextValue);
+  } else {
+    return patchAttr(el, key, nextValue);
+  }
+}
